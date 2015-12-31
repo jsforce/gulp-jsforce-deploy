94d3916b11a8abb6efe4b4b77224bc956c1c8655
James Kent <james.kent@vertic.com.au>
2015-12-31 15:34:55 +1100

    Updated index.js to report failures.
    Have added support for scenarios where the package deployment has failed.  This is useful where subsequent tasks require the successful deployment of the package.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var meta = require('jsforce-metadata-tools');
 
+var pluginName = 'gulp-jsforce-deploy';
+
 module.exports = function(options) {
   return through.obj(function(file, enc, callback) {
     var err;
@@ -10,14 +12,19 @@ module.exports = function(options) {
       return callback(null, file);
     }
     if (file.isStream()) {
-      err = new gutil.PluginError('gulp-jsforce-deploy', 'Stream input is not supported');
+      err = new gutil.PluginError(pluginName, 'Stream input is not supported');
       return callback(err);
     }
     options.logger = gutil;
     meta.deployFromZipStream(file.contents, options)
       .then(function(res) {
         meta.reportDeployResult(res, gutil, options.verbose);
-        callback();
+        if (res.success)
+          callback();
+        else {
+          err = new gutil.PluginError(pluginName, 'An error occurred deploying the package.');
+          callback(err, res);
+        }
       })
       .catch(function(err) {
         callback(err);
