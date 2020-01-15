4bc6421db4217ae1f5eb20952266dfb503c21537
Aaron Huggins <ahuggins@nethealthshops.com>
2020-01-15 09:44:41 -0600

    Completed retrieve and extract functionality

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 'use strict'
-var PluginError = require('plugin-error')
-var fancyLog = require('fancy-log')
-var through = require('through2')
-var meta = require('jsforce-metadata-tools')
+const meta = require('jsforce-metadata-tools')
+const PluginError = require('plugin-error')
+const decompress = require('decompress')
+const fancyLog = require('fancy-log')
+const through = require('through2')
+const parser = require('xml2js')
+const Vinyl = require('vinyl')
 
 const deploy = function deploy (options) {
   return through.obj(function (file, enc, callback) {
     if (file.isNull()) {
       return callback(null, file)
     }
+
     if (file.isStream()) {
       return callback(
         new PluginError('gulp-jsforce-deploy', 'Stream input is not supported')
       )
     }
+
     options.logger = Object.assign({ log: fancyLog }, fancyLog)
+
     meta
       .deployFromZipStream(file.contents, options)
       .then(function (res) {
         meta.reportDeployResult(res, options.logger, options.verbose)
+
         if (!res.success) {
           return callback(
             new PluginError('gulp-jsforce-deploy', 'Deploy Failed.')
           )
         }
+
         callback(null, file)
       })
       .catch(function (err) {
@@ -32,6 +40,88 @@ const deploy = function deploy (options) {
   })
 }
 
-deploy.prototype.retrieve = function retrieve (options) {}
+deploy.deploy = function deploy(options) {
+  return this.apply(this, options)
+}
+
+deploy.retrieve = function retrieve (options) {
+  return through.obj(function (file, enc, callback) {
+    if (file.isNull()) {
+      return callback(null, file)
+    }
+
+    if (file.isStream()) {
+      return callback(
+        new PluginError('gulp-jsforce-deploy', 'Stream input is not supported')
+      )
+    }
+
+    options.logger = Object.assign({ log: fancyLog }, fancyLog)
+
+    parser.parseStringPromise(file.contents.toString('utf8'))
+      .then(function (dom) {
+        delete dom.Package.$
+        options.unpackaged = dom.Package
+        return meta.retrieve(options)
+      })
+      .then(function (res) {
+        meta.reportRetrieveResult(res, options.logger, options.verbose)
+
+        if (!res.success) {
+          return callback(
+            new PluginError('gulp-jsforce-deploy', 'Retrieve Failed.')
+          )
+        }
+
+        callback(null, new Vinyl({
+          cwd: file.cwd,
+          base: file.base,
+          path: file.base + '/' + (options.filename || 'package.zip'),
+          contents: new Buffer(res.zipFile, 'base64')
+        }))
+      })
+      .catch(function (err) {
+        callback(err)
+      })
+  })
+}
+
+deploy.extract = function extract (options = {}) {
+  return through.obj(function (file, enc, callback) {
+    if (file.isNull()) {
+      return callback(null, file)
+    }
+
+    if (file.isStream()) {
+      return callback(
+        new PluginError('gulp-jsforce-deploy', 'Stream input is not supported')
+      )
+    }
+
+    // options.logger = Object.assign({ log: fancyLog }, fancyLog)
+    const extractFactory = this
+
+    decompress(file.contents)
+      .then(function (files) {
+        files.forEach((file) => {
+          const path = file.path.replace(/^unpackaged\//, '')
+
+          extractFactory.push(new Vinyl({
+            cwd: './',
+            path,
+            contents: file.data
+          }))
+
+          if (options.verbose) fancyLog(`Extracted file '${path}'`)
+        })
+
+        fancyLog(`Finished extracting ${files.length} files.`)
+        callback()
+      })
+      .catch(function (err) {
+        callback(err)
+      })
+  })
+}
 
 module.exports = deploy
